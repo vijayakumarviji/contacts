61b2903881121f1cdc591262be299b782e24e1dc
Vijayakumar P <vijayakumar859@gmail.com>
2019-05-15 07:21:21 +0530

    Added Api Documentation (swagger-ui-express)

--- src/app.js
+++ src/app.js
@@ -1,6 +1,9 @@
 import express from 'express';
+const swaggerUi = require('swagger-ui-express');
+
 const app = express();
 const router = express.Router();
+const swaggerDocument = require('./swagger.json');
 
 import routes from './routes';
 import { connectDb } from './data-accessor/connectdb';
@@ -10,7 +13,11 @@ import log from './utils/logger';
 global.logger = log;
 
 export async function main() {
+    // Adding Api Documentation
+    app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+    // Adding Routes
     app.use(routes(router));
+    // Making Connection to DB
     await connectDb();
     app.listen(Config.SERVER.PORT, () => console.log(`App listening on port ${Config.SERVER.PORT}`));
     return {};

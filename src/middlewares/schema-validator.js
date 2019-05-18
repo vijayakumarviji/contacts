e183c21599264b89a8aec1431256220b1ae1932c
Vijayakumar P <vijayakumar859@gmail.com>
2019-05-18 10:16:42 +0530

    Added response validation module

--- src/middlewares/schema-validator.js
+++ src/middlewares/schema-validator.js
@@ -20,48 +20,79 @@ const SchemaValidateMiddleWare = {
                 const schemaValidations = [];
                 if (schema.body)
                     schemaValidations.push(
-                        validate(req, 'body', schema.body, validateOptions)
+                        validate(req, 'body', schema.body, validateOptions, 'Request')
                     );
                 if (schema.params)
                     schemaValidations.push(
-                        validate(req, 'params', schema.params, validateOptions)
+                        validate(req, 'params', schema.params, validateOptions, 'Request')
                     );
                 if (schema.query)
                     schemaValidations.push(
-                        validate(req, 'query', schema.query, validateOptions)
+                        validate(req, 'query', schema.query, validateOptions, 'Request')
                     );
                 Promise.all(schemaValidations)
                     .then(() => {
                         next();
                     })
                     .catch(err => {
-                        next(err);
+                        res.status(err.responseCode).json(err)
                     });
             } else {
                 next();
             }
         }
+    },
+    responseSchemaValidator: () => {
+        const validateOptions = {
+            abortEarly: false,
+            allowUnknown: true,
+            stripUnknown: true
+        };
+        return async (req, res, next) => {
+            const route = req.route.path;
+            const method = req.method.toLowerCase();
+            const schemaPath = method + route;
+            if (_.has(Schemas, schemaPath)) {
+                const schema = _.get(Schemas, schemaPath);
+                try {
+                    let validatedResponse = req.result;
+                    if (schema.response)
+                        validatedResponse = await validate({ result: JSON.parse(JSON.stringify(req.result)) }, 'result', schema.response, validateOptions, 'Response');
+                    res.send(validatedResponse);
+                } catch (err) {
+                    res.status(err.responseCode).json(err)
+                }
+            } else {
+                console.log(req.result)
+                res.send(req.result);
+            }
+        }
     }
 };
 
-const validate = (req, key, schema, validateOptions) => new Promise((resolve, reject) => {
+const validate = (req, key, schema, validateOptions, requestType) => new Promise((resolve, reject) => {
     joi.validate(
         req[key],
         schema,
         validateOptions,
         (err, validatedResponse) => {
             if (!err) {
-                req[key] = validatedResponse;
-                resolve();
+                if (requestType == 'Response') {
+                    resolve(validatedResponse);
+                } else {
+                    req[key] = validatedResponse;
+                    resolve();
+                }
             } else {
-                Logger.error(
-                    'error',
-                    'schemaValidator',
-                    'validate',
-                    'validate Error',
-                    err
-                );
-                reject(err);
+                logger.log({
+                    level: 'error',
+                    message: `${requestType}-validate-${err.message}`,
+                })
+                reject({
+                    responseCode: 400,
+                    responseMessageCode: `BadRequest${requestType}ValidationFailed`,
+                    responseMessage: `${requestType} Validation Failed`
+                });
             }
         }
     );
